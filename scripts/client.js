990964993b681f8af437edc8c067dac999636a45
Rekka <rekkabell@gmail.com>
2019-12-28 10:00:57 -0500

    page 8

--- scripts/client.js
+++ scripts/client.js
@@ -7,7 +7,7 @@ function Client () {
   this._viewer = document.createElement('div')
   this._viewer.id = 'viewer'
   this.index = 0
-  this.pages = 7
+  this.pages = 8
 
   this.install = (host) => {
     this.el.appendChild(this._controls)
